9106258f3f1215e704ef3f99a254ae379f882756
Grzegorz Maciborek <Maciborek.grzegorz@gmail.com>
2021-08-31 21:00:46 +0200

    next section about "How Reack works"

--- project-one/src/App.js
+++ project-one/src/App.js
@@ -1,32 +1,12 @@
-import { Route, Switch, Redirect } from "react-router-dom";
+import React from 'react';
 
-import Layout from "./components/layout/Layout";
-import AllQuotes from "./pages/AllQuotes";
-import NewQuote from "./pages/NewQuote";
-import NotFound from "./pages/NotFount";
-import QuoteDetails from "./pages/QuoteDetails";
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <Layout>
-      <Switch >
-        <Route path="/" exact>
-          <Redirect to="/quotes" />
-        </Route>
-        <Route path='/quotes' exact>
-          <AllQuotes />
-        </Route>
-        <Route path='/quotes/:quoteId'>
-          <QuoteDetails />
-        </Route>
-        <Route path='/new-quote'>
-          <NewQuote />
-        </Route>
-        <Route path='*'>
-          <NotFound />
-        </Route>
-      </Switch>
-    </Layout>
+    <div className="app">
+      <h1>Hi there!</h1>
+    </div>
   );
 }
 
