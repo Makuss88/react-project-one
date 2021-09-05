5ddcc957df2c84cabd404f0b15f5ed839efddf68
Grzegorz Maciborek <Maciborek.grzegorz@gmail.com>
2021-09-05 23:19:12 +0200

    end demo section aboun "How react works, using Memo, useMemo, useCallback" next commit => change subfolder about section

--- project-one/src/App.js
+++ project-one/src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+
+import Button from './components/UI/Button/Button'
 
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
 
-function App() {
+const App = () => {
+
+  const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
+
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph(prevShowParagraph => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(!allowToggle);
+  };
+
+  console.log("App");
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <Button onClick={allowToggleHandler} className={"button"}>maybe toggle??</Button>
+      <Button onClick={toggleParagraphHandler} className={"button"}>Toggle paragraph</Button>
+      <DemoOutput show={showParagraph} />
     </div>
   );
 }
---
+++ project-one/src/components/Demo/MyParagraph.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MyParagraph = (props) => {
+
+  console.log("MyParagraph");
+  return <p>{props.children}</p>
+}
+
+export default MyParagraph;
