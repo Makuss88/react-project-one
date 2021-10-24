fdad2b7c551b7d901784c888d28b390b3dd47a64
Makuss88 <maciborek.grzegorz@gmail.com>
2021-10-24 17:18:15 +0200

    new Joke

--- project-one/src/014section-database/assets/Text.js
+++ project-one/src/014section-database/assets/Text.js
@@ -1,1 +1,3 @@
 export const BASE_URL = "https://swapi.dev/api/films/";
+
+export const BASE_URL_JOKE = "https://v2.jokeapi.dev/joke/Any?type=single";
--- project-one/src/App.js
+++ project-one/src/App.js
@@ -1,13 +1,13 @@
 import React, { useState, Fragment } from 'react';
 import axios from 'axios';
 
-import { BASE_URL } from './014section-database/assets/Text'
+import { BASE_URL, BASE_URL_JOKE } from './014section-database/assets/Text'
 
 import MoviesList from './014section-database/components/MoviesList';
 import './App.css';
 
 const App = () => {
-
+  const [joke, setJoke] = useState('');
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -27,12 +27,20 @@ const App = () => {
     setIsLoading(false);
   }
 
+  const axiosJokeHandlers = () => {
+    axios.get(BASE_URL_JOKE).then((response) => { setJoke(response.data.joke) })
+  }
+
   return (
     <Fragment>
+
+
       <section>
+        <button onClick={axiosJokeHandlers}>Fetch JOKES!</button>
         <button onClick={fetchMoviesHandlers}>Fetch Movies</button>
       </section>
       <section>
+        {joke}
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
         {!isLoading && movies.length === 0 && <p>Found no movies...</p>}
         {isLoading && <p> Loading...</p>}
