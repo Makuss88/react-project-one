fb82ecfbee4eb221926198ae17ced122879ce0b3
Makuss88 <maciborek.grzegorz@gmail.com>
2021-11-30 23:51:25 +0100

    add useEffect, and useCallback

--- project-one/src/014section-database/assets/Text.js
+++ project-one/src/014section-database/assets/Text.js
@@ -1,3 +1,2 @@
 export const BASE_URL = "https://swapi.dev/api/films/";
-
 export const BASE_URL_JOKE = "https://v2.jokeapi.dev/joke/Any?type=single";
--- project-one/src/App.js
+++ project-one/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, Fragment, useEffect, useCallback } from 'react';
 import axios from 'axios';
 
 import { BASE_URL, BASE_URL_JOKE } from './014section-database/assets/Text'
@@ -10,40 +10,85 @@ const App = () => {
   const [joke, setJoke] = useState('');
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null)
 
-  async function fetchMoviesHandlers() { // założyłem async i away
+  const axiosMoviesHandlers = useCallback(async () => { // założyłem async i away
     setIsLoading(true);
-    const response = await axios.get(BASE_URL);
-    const data = await response.data.results;
-    const transform = data.map(movieData => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovies(transform);
+    setError(null);
+    try {
+      const response = await axios.get(BASE_URL);
+      const data = await response.data.results;
+
+      const transform = data.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+
+      setMovies(transform);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
+  }, [])
+
+  useEffect(() => {
+    axiosMoviesHandlers();
+  }, [axiosMoviesHandlers]);
+
+  const fetchMoviesHandlers = () => {
+    fetch(BASE_URL)
+      .then(response => response.json())
+      .then(data => {
+        const transform = data.results.map(movieData => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        setMovies(transform);
+      }
+      );
   }
 
   const axiosJokeHandlers = () => {
+
+    setIsLoading(true);
+
     axios.get(BASE_URL_JOKE).then((response) => { setJoke(response.data.joke) })
+
+    setIsLoading(false);
+  }
+
+  let contetn = <p>Found no movies...</p>;
+
+  if (movies.length > 0) {
+    contetn = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    contetn = <p>Error: {error}</p>;
+  }
+
+  if (isLoading) {
+    contetn = <p>Loading...</p>;
   }
 
   return (
     <Fragment>
-
-
       <section>
         <button onClick={axiosJokeHandlers}>Fetch JOKES!</button>
         <button onClick={fetchMoviesHandlers}>Fetch Movies</button>
+        <button onClick={axiosMoviesHandlers}>Axiox Movies</button>
       </section>
       <section>
         {joke}
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies...</p>}
-        {isLoading && <p> Loading...</p>}
+        {contetn}
       </section>
     </Fragment>
   );
