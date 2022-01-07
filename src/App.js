4f3be7274df41fa1c96ca21930535ebf1b9a26b1
Makuss88 <maciborek.grzegorz@gmail.com>
2022-01-07 18:23:19 +0100

    to build

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 
 // import Ap from './009section-store/Ap';
 
-// import Ap from './011section-mealsShop/Ap';  //update!!
-// import './011section-mealsShop/index.css';
+import Ap from "./011section-mealsShop/Ap"; //update!!
+import "./011section-mealsShop/index.css";
 
 // import Ap from './012section-howReactWorks'
 
@@ -12,8 +12,8 @@ import React from "react";
 // import Ap from './016section-Forms/Ap';
 // import './016section-Forms/index.css';
 
-import Ap from "./018section-redux/Ap";
-import "./018section-redux/index.css";
+// import Ap from "./018section-redux/Ap";
+// import "./018section-redux/index.css";
 
 const App = () => {
   return (
